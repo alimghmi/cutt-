dfd54c60f1d79e3d90bd0bbef44327d1727a89f2
Ali Moghimi <mghmiali@gmail.com>
2022-05-21 02:47:52 +0430

    add index and redirect views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import Link
+
+class IndexPage(TemplateView):
+    
+    def get(self, request):
+        return HttpResponse('It worked')
+
+
+class RedirectPage(TemplateView):
+    
+    def get(self, request, **kwargs):
+        try:
+            slug = self.kwargs.get('slug', None)
+            query = Link.objects.get(slug=slug)
+
+            if query.is_enabled:
+                query.visits_count += 1
+                query.save()
+                return redirect(query.origin)
+            else:
+                return redirect('index')
+        except:
+            return redirect('index')
