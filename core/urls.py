9efada10bd9063103d0393956eee09b68b5b9e3e
Ali Moghimi <mghmiali@gmail.com>
2022-05-21 15:18:55 +0430

    add stats endpoint to url

--- core/urls.py
+++ core/urls.py
@@ -4,13 +4,15 @@ from .views import (
     IndexPage, 
     RedirectPage, 
     CreateShortenURLAPI,
-    DeleteShortenURLAPI
+    DeleteShortenURLAPI,
+    StatsShortenURLAPI
 )
 
 urlpatterns = [
     path('', IndexPage.as_view(), name='index'),
     path('api/create', CreateShortenURLAPI.as_view(), name='create_shorturl'),
     path('api/delete', DeleteShortenURLAPI.as_view(), name='delete_shorturl'),
+    path('api/stats', StatsShortenURLAPI.as_view(), name='stats_shorturl'),
     # path('api/all', ShortenURLAPI.as_view(), name='redirect'),
     # path('api/update', ShortenURLAPI.as_view(), name='redirect'),
     path('<str:slug>', RedirectPage.as_view(), name='redirect'),
